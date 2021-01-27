59d5e1bf721a19813881df614718cd5973f1824a
Geethababu <geethabsp07@gmail.com>
2021-01-27 22:31:32 +0530

    Add files via upload

---
+++ EmployeePromotion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium_1
+{
+    class EmployeePromotion : Employee
+    {
+
+        public static Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+
+
+        public void FindEmployeeByID()
+        {
+            int id = -1;
+            try
+            {
+                Console.WriteLine("Please enter the employee ID : ");
+                id = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("ID should be a number ");
+                FindEmployeeByID();
+                return;
+            }
+
+            if (employees.ContainsKey(id))
+            {
+                Console.WriteLine(employees[id].ToString());
+            }
+            else
+            {
+                Console.WriteLine("Employee ID: {0} not found in Promotion List", id);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Medium_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeePromotion p = new EmployeePromotion();
+            int check = 1;
+            while (check == 1)
+            {
+                Employee emp = new Employee();
+                emp.TakeEmployeeDetailsFromUser();        
+                    Console.WriteLine("To continue entering employee details enter 1, to exit enter 0");
+                    check = Convert.ToInt32(Console.ReadLine());
+            }
+            p.FindEmployeeByID();
+            Console.ReadKey();
+
+        }
+    }
+}
